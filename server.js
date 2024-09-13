7724808c2d332f4682b6953ae78827db4139222b
Frank Stump <stump9219@gmail.com>
2024-09-13 10:13:44 -0500

    commit

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const port = 3000;
 
 // Sample catalog data
 const catalogItems = [
-  { id: 1, name: 'Eragon', description: 'A boy gets a dragon and goes on an adventure.', image: 'bookstore/public/images/Eragon.jpg'},
-  { id: 2, name: 'Eldest', description: 'That boy and his dragon grow up and continue on their adventure.', image: 'bookstore/public/images/Eldest.jpg'},
-  { id: 3, name: 'Inheritance', description: 'The adventure continues, just with more people and dragons.', image: 'bookstore/public/images/inheritance.jpg'},
-  { id: 4, name: 'Murtagh', description: 'The brother of the original character also found a dragon and began his adventures.', image: 'bookstore/public/images/murtagh.jpg'},
-  { id: 5, name: 'A Tail of Two Cities', description: "I've never read this book so I don't know.", image: 'bookstore/public/images/tailoftwocities.jpg'},
-  { id: 6, name: 'The Little Prince', description: "I think this is a French book.", image: 'bookstore/public/images/Littleprince.jpg'},
-  { id: 7, name: 'The Alchemist', description: "Brotherhood was better than full metal.", image: 'bookstore/public/images/TheAlchemist.jpg'},
-  { id: 8, name: 'HP and the Philosophers Stone', description: "Didn't know this book sold over 100 million copies.", image: 'bookstore/public/images/hpandthestone.jpg'},
-  { id: 9, name: 'Dream of the Red Chamber', description: 'This is apparently a mystery book that sold really well.', image: 'Dreamoftheredchamber.jpg'},
-  { id: 10, name: 'The Hobbit.', description: 'The best book on this website to be honest', image: 'thehobbit.jpg'},
-  { id: 11, name: "Alice's Adventure in the Wonderland", description: 'alice had depression and this was all in her head to try and cope with reality.', image: 'bookstore/public/images/aliceinwonderland.jpg'},  
+  { id: 1, name: 'Eragon', description: 'A boy gets a dragon and goes on an adventure.'},
+  { id: 2, name: 'Eldest', description: 'That boy and his dragon grow up and continue on their adventure.',},
+  { id: 3, name: 'Inheritance', description: 'The adventure continues, just with more people and dragons.',},
+  { id: 4, name: 'Murtagh', description: 'The brother of the original character also found a dragon and began his adventures.',},
+  { id: 5, name: 'A Tail of Two Cities', description: "I've never read this book so I don't know.",},
+  { id: 6, name: 'The Little Prince', description: "I think this is a French book.",},
+  { id: 7, name: 'The Alchemist', description: "Brotherhood was better than full metal.",},
+  { id: 8, name: 'HP and the Philosophers Stone', description: "Didn't know this book sold over 100 million copies.",},
+  { id: 9, name: 'Dream of the Red Chamber', description: 'This is apparently a mystery book that sold really well.',},
+  { id: 10, name: 'The Hobbit.', description: 'The best book on this website to be honest',},
+  { id: 11, name: "Alice's Adventure in the Wonderland", description: 'alice had depression and this was all in her head to try and cope with reality.',},  
   // Add more items here
 ];
 
